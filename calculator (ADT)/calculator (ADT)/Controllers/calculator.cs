8ae01cb015402915e2bb71396bc9f0f2d303cbc1
ST10037089-Alisa Diya Thool <104021252+alisadthool@users.noreply.github.com>
2024-03-16 20:07:28 +0200

    My Calc
    1st commit

---
+++ calculator (ADT)/calculator (ADT)/Controllers/calculator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace calculator__ADT_.Controllers
+{
+    public class calculator : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculate(string expression)
+        {
+            try
+            {
+                var result = new System.Data.DataTable().Compute(expression, null);
+                return Json(result);
+            }
+            catch
+            {
+                return Json("Error");
+            }
+        }
+    }
+}
+ 
